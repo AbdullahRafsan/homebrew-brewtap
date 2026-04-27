2996b51846aa19abad91b779a2283e933dec21af
Abdullah Rafsan <rafsanabdullah88@gmail.com>
2026-04-27 11:50:03 +0600

    Updated smali.zip

--- smali.rb
+++ smali.rb
@@ -3,7 +3,7 @@ class Smali < Formula
   homepage "https://github.com/AbdullahRafsan/homebrew-brewtap"
   url "https://github.com/AbdullahRafsan/homebrew-brewtap/raw/refs/heads/main/smali.zip"
   version "2.5.2"
-  sha256 "80970c8a934739d4071ef57126e5f427521814910277ddb69ab644ba835fabac"
+  sha256 "c86181c84ec6844aa8a4331d667313f39b12040fd6f99a15a0871e8ecfb7a03b"
   license "MIT"
 
   depends_on "openjdk"
@@ -14,8 +14,8 @@ class Smali < Formula
     # system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     prefix.install "smali", "baksmali", "smali.jar", "baksmali.jar"
-    bin.install "#{prefix}/smali"
-    bin.install "#{prefix}/baksmali"
+    bin.install_symlink "#{prefix}/smali"
+    bin.install_symlink "#{prefix}/baksmali"
   end
 
   test do
