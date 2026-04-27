201bfd7ce53e2321a857e5b4338d83559718cf1c
Abdullah Rafsan <rafsanabdullah88@gmail.com>
2026-04-27 11:24:49 +0600

    Changed smali.zip

---
+++ smali.rb
@@ -0,0 +1,27 @@
+class Smali < Formula
+  desc "Program to disassemble Android dex code"
+  homepage "https://github.com/AbdullahRafsan/homebrew-brewtap"
+  url "https://github.com/AbdullahRafsan/homebrew-brewtap/raw/refs/heads/main/smali.zip"
+  version "2.5.2"
+  sha256 "e20fb58ab096df9c1242b2ca7e6284997cd23a4d25bdcd3fc5305409f77cd224"
+  license "MIT"
+
+  depends_on "openjdk"
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    prefix.install "smali", "baksmali", "smali.jar", "baksmali.jar"
+    bin.install "#{prefix}/smali"
+    bin.install "#{prefix}/baksmali"
+  end
+
+  test do
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "smali -v"
+    system "baksmali -v"
+  end
+end
